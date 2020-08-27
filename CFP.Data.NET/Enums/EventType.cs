ffb179b153f716a3779a8b749071445e99f39741
Rick van den Bosch <rickvdbosch@outlook.com>
2020-08-27 11:06:28 +0200

    Added EventType enumeration

---
+++ CFP.Data.NET/Enums/EventType.cs
@@ -0,0 +1,10 @@
+﻿namespace CFP.Data.NET.Enums
+{
+    /// <summary>The type of the event (for instance: In-person, or Online)</summary>
+    public enum EventType
+    {
+        InPerson,
+        Online,
+        Hybrid
+    }
+}
