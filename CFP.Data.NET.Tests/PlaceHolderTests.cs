cdad98f1dd4c70f04f87262046d980fe09fb48ad
Rick van den Bosch <rickvdbosch@outlook.com>
2020-08-28 08:53:44 +0200

    Added PlaceHolderTests class

---
+++ CFP.Data.NET.Tests/PlaceHolderTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace CFP.Data.NET.Tests
+{
+    public class PlaceHolderTests
+    {
+        [Fact]
+        public void Given_When_Then()
+        {
+            // TODO implement decent tests as soon as CFP.Data.NET has classes with functionality
+            Assert.True(true);
+        }
+    }
+}
